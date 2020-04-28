590e0383a02a59f798ee58bb04ce260b4886006d
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-04-28 14:22:01 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,31 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "김" + "예" + "은";
+            Console.Write("Hello C#");
+            int add = 2;
+            //주석
+            /*
+             * 
+             * 여러줄 주석
+             */
+            Console.WriteLine("Hello World");
+            Console.Write("Hello world");
+            Console.Write("Hello world");
+
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(true);
+
         }
     }
 }
